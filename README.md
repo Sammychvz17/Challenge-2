8a7f896312f7c8d88fa65b40693495d622b1e9f4
Sammychvz17 <sammy_chvz17@outlook.com>
2025-11-16 18:36:44 -0600

    added class headers and struct for Data and Node

--- README.md
+++
@@ -1,3 +0,0 @@
-# Challenge-2
-
-This is an example of a stack and linked list combined. 
--- queue.h
+++ queue.h
@@ -5,3 +5,75 @@
 *           based queue ADT that can operate in either
 *           FIFO or LIFO mode.
 ************************************************* */
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <string>
+using std::string;
+
+/**************************************************
+*  Struct: Data
+*  Purpose: Store the id and information for each
+*           item in the queue.
+************************************************* */
+struct Data {
+    int id;
+    string information;
+};
+
+/**************************************************
+*  Enum: QueueType
+*  Purpose: Selects the algorithm used by the queue:
+*           FIFO or LIFO.
+************************************************* */
+enum QueueType {
+    FIFO = 0,
+    LIFO = 1
+};
+
+/**************************************************
+*  Class: Queue
+*  Purpose: Linked-list based queue that can behave
+*           as either FIFO or LIFO depending on the
+*           constructor flag.
+************************************************* */
+class Queue {
+
+public:
+    /**********************
+    *  Constructors / Destructor
+    ***********************/
+
+
+    /**********************
+    *  Mutators
+    ***********************/
+    
+    /**********************
+    *  Accessors
+    ***********************/
+
+private:
+    /**************************************************
+    *  Struct: Node
+    *  Purpose: Internal linked list node that stores
+    *           Data and pointers to neighboring nodes.
+    ************************************************* */
+    struct Node {
+    Data data;
+    Node *next;
+    Node *prev;
+    };
+
+
+    /**********************
+    *  Attributes
+    ***********************/
+
+};
+
+
+
+
+#endif // QUEUE_H
